ac06446048ee797d67a371bb6146d7ff130f7812
mvajhi <mvajhimv@gmail.com>
2024-12-05 11:28:38 +0330

    end of RR

--- ca3/trap.c
+++ ca3/trap.c
@@ -104,7 +104,7 @@ trap(struct trapframe *tf)
   // If interrupts were on while locks held, would need to check nlock.
   if(myproc() && myproc()->state == RUNNING &&
      tf->trapno == T_IRQ0+IRQ_TIMER)
-    if(ticks % 100 == 0)
+    if(ticks % 5 == 0)
       yield();
 
   // Check if the process has been killed since we yielded
